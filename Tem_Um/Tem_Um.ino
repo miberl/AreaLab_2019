f9c7122d0eccfa090101de4c0a4ea702cd43c51a
Michael B <47005505+michaelberli@users.noreply.github.com>
2019-05-28 09:33:15 +0200

    Add files via upload

--- Tem_Um/Tem_Um.ino
+++ Tem_Um/Tem_Um.ino
@@ -1,20 +1,31 @@
 #include <DHT.h>
 #include "DHT.h"
-#define DHTPIN 7
+#define DHTPIN 5
 #define DHTTYPE DHT11
+int letturainput=8;
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  digitalWrite(13,HIGH);
   Serial.begin(9600);
+
 }
 
 void loop() {
-  int t=dht.readTemperature();
-  int h=dht.readHumidity();
-  Serial.print("Temperatura: ");
-  Serial.println(t);
-  Serial.print("Umidita': ");
-  Serial.print(h);
-  Serial.println(" %");
+    int valoreletto=digitalRead(letturainput);
+    if (valoreletto==1){
+      int t=dht.readTemperature();
+      int h=dht.readHumidity();
+      Serial.print(" Temperatura: ");
+      Serial.print(t);
+      Serial.println("°C");
+      Serial.print(" Umidita': ");
+      Serial.print(h);
+      Serial.println("%");
+      Serial.println();
+  }
   delay(2000);
+  Serial.println(" Lettura");
+  delay (1000);
+  
 }
