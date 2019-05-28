c5b952ef2487a1e2f7f327c37ad97c95b4ad11dc
Michael B <47005505+michaelberli@users.noreply.github.com>
2019-05-28 09:49:13 +0200

    Add files via upload

---
+++ Lettura_Seriale_Java/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import com.fazecast.jSerialComm.*;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        SerialPort sp = SerialPort.getCommPort("/dev/cu.usbmodem142101"); //Porta seriale dal quale legge l'input
+        sp.setComPortParameters(9600, 8, 1, 0); //Parametri default di Arduino
+        sp.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0); // Default lettura byte
+
+        if (sp.openPort()) {
+            System.out.println("In trasmissione");
+        } else {
+            System.out.println("Errore: porta seriale non collegata!");
+            return;
+        }
+        InputStream in = sp.getInputStream();
+        try
+        {
+            for (int j = 0; j < 5000; j++) //legge 5000 byte
+                System.out.print((char)in.read()); //Converte il byte appena letto in char
+            in.close();                             //Chiude il canale di memorizzazione dei valori letti
+        } catch (Exception e) { e.printStackTrace(); }
+        sp.closePort();                            //Chiude il collegamento seriale da Arduino
+
+    }
+
+}
